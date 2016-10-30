52ef648c1b30b476a4f7e88682c7eacb6f990555
Tulio Troche <tgtroche@gmail.com>
2016-10-30 11:31:39 -0400

    second commit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+  article: Article;
+
+  constructor() {
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
+   }
+
+  voteUp(){
+    this.article.votes += 1;
+    return false;
+  }
+
+  voteDown(){
+    this.article.votes -= 1;
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
